951403dda5593f0e38153c73aa8c30c976166f6e
prajjwaldangal <oscreader@OSC>
2018-02-26 17:39:16 -0700

    make main work

--- main.c
+++ main.c
@@ -1,52 +1,31 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <stdlib.h>
+#include <math.h>
 
 int n;
-
-void pickup_forks(int p) 
-{
-	// lock mutex
-	pthread_mutex_lock(&available);
-	state[p] = eating;
-	// random (60 - 180 secs)
-	// unlock mutex
-}
-
-int check_others(int p)
-{
-	// if sideways in hungry state -> convert to eating
-	if (state[i] == 
-}
-
-void return_forks(int p)
-{
-	// unlock mutex
-	pthread_mutex_unlock(&available);
-	state[p] = thinking;
-	check_others(p);
-}
+typedef enum {thinking, eating, hungry} state;
+state State[9999999];
+pthread_mutex_t available;
+pthread_cond_t condition[9999999];
 
 int main(int argc, char * argv[])
 {
+	printf("Note: no. of philosophers input to the program must be less than 9999999\n");
 	n = (int) argv[1];
 	printf("No. of philosophers in the program is %d", n);
-	typedef enum {thinking, hungry, eating} state[n];
-	pthread_cond_t condition[n];
+	
 	// start with thinking for all philosophers
 	for(int i=0; i<n; i++) {
-		state[i] = thinking;
-		if(pthread_cond_init(condition[i], pthread_condattr_default) != 0) {
+		State[i] = thinking;
+		if( pthread_cond_init(&condition[i], NULL) != 0) {
          		perror("pthread_cond_init");
          		exit(1);
          	}
      	}
+     	while(1) {
+     		
+     	}
      	
-     	//for (i=0; it)[i]),pthread_attr_default,
-	//   	       philosopher, &((tab->self)[i]))!= 0) {
-         //perror("pthread_create");
-         //exit(1);}
-	//}
-	//pthread_create();
 	return 0;			
 }
--- sample.c
+++ sample.c
@@ -1,27 +1,23 @@
-#include <stdio.h>
-
-int n;
-
-int main(int argc, char * argv[])
+void pickup_forks(int p)
 {
-	n = (int) argv[1];
-	printf("No. of philosophers in the program is %d", n);
-	typedef enum {thinking, hungry, eating} state[n];
-	pthread_cond_t condition[n];
-	// start with thinking for all philosophers
-	for(int i=0; i<n; i++) {
-		state[i] = thinking;
-		if(pthread_cond_init(condition[i], pthread_condattr_default) != 0) {
-         		perror("pthread_cond_init");
-         		exit(1);
-         	}
-     	}
-     	
-     	//for (i=0; it)[i]),pthread_attr_default,
-	//   	       philosopher, &((tab->self)[i]))!= 0) {
-         //perror("pthread_create");
-         //exit(1);}
-	//}
-	//pthread_create();
-	return 0;			
+	// lock mutex
+	pthread_mutex_lock(&available);
+	state[p] = eating;
+	// random (60 - 180 secs)
+	// unlock mutex
+}
+
+int check_others(int p)
+{
+	// if sideways in hungry state -> convert to eating
+	//if (state[abs(i-1)] == hungry)
+	return 0;
+}
+
+void return_forks(int p)
+{
+	// unlock mutex
+	pthread_mutex_unlock(&available);
+	state[p] = thinking;
+	check_others(p);
 }
