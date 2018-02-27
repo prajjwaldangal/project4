5e4b6ae83852ca7ca6a3e06d55c53615fc4d1580
prajjwaldangal <oscreader@OSC>
2018-02-26 18:37:57 -0700

    create threads

--- main.c
+++ main.c
@@ -3,18 +3,39 @@
 #include <stdlib.h>
 #include <math.h>
 
-int n;
+int n=99999; // simply placeholder value
 typedef enum {thinking, eating, hungry} state;
 state State[9999999];
 pthread_mutex_t available;
 pthread_cond_t condition[9999999];
 
-int main(int argc, char * argv[])
-{
-	printf("Note: no. of philosophers input to the program must be less than 9999999\n");
-	n = (int) argv[1];
-	printf("No. of philosophers in the program is %d", n);
+void printState(void) {
+	printf("Current state:\n");
+	printf("0 means thinking, 1 means eating, 2 means hungry\n");
+	printf("Philosopher number         State\n");
+	for (int i = 0; i < n; i++) {
+		printf("%d                 %d\n", i, State[i]);
+	}
+}
+
+void think(int p_n) {
 	
+}
+
+void *worker (void * p_n) {
+	int * k = (int *) p_n;
+	int p = *k;
+	while(1) {
+		think(p);	
+	}
+}
+
+int main(int argc, const char * argv[])
+{
+	printf("*************************************************\n\nNote: I am assuming that the no. of philosophers input to the program does not exceed 9999999.\n*************************************************\n");
+	printf("Enter number of philosophers for the program\n");
+	scanf("%d", &n);
+
 	// start with thinking for all philosophers
 	for(int i=0; i<n; i++) {
 		State[i] = thinking;
@@ -23,9 +44,16 @@ int main(int argc, char * argv[])
          		exit(1);
          	}
      	}
-     	while(1) {
-     		
+     	pthread_t threads[n];
+     	pthread_attr_t attrs[n];
+     	for (int i=0; i<n; i++) {
+		pthread_attr_init(&attrs[i]);
+	}
+     	int * pass;
+     	for (int i=0; i<n; i++) {
+     		pass = &i;
+     		pthread_create(&threads[i], &attrs[i], worker, (void *) pass); 
      	}
-     	
+     	printState();
 	return 0;			
 }
